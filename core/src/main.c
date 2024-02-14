794f685e3152a37a77404e0977d6759c7184d3bc
Thiago Prado <thiagopereiraprado@gmail.com>
2024-02-14 14:51:42 -0300

    Use HAL to configure clock

--- core/src/main.c
+++ core/src/main.c
@@ -4,7 +4,6 @@
 #include "stm32f1xx.h"
 #include "core_cm3.h"
 
-#include "rcc.h"
 #include "timer.h"
 
 #include "infrared.h"
@@ -20,9 +19,37 @@
 static volatile uint32_t timer_counter = 0;
 
 /** Prototypes ---------------------------------------------------- */
+static void clock_config(void);
 static void timer_callback(void);
 
 /** Internal functions -------------------------------------------- */
+/**
+ * @brief MCU clock configuration.
+ */
+static void clock_config(void)
+{
+    RCC_OscInitTypeDef RCC_OscInitStruct = { 0 };
+    RCC_ClkInitTypeDef RCC_ClkInitStruct = { 0 };
+
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+    RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+    RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
+    RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+    RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
+    HAL_RCC_OscConfig(&RCC_OscInitStruct);
+
+    /** Initializes the CPU, AHB and APB buses clocks
+     */
+    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+    HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2);
+}
+
 /**
  * @brief Timer callback.
  * 
@@ -37,7 +64,7 @@ int main(void) {
     uint32_t timeshot = 0;
 
     HAL_Init();
-    rcc_clock_init();
+    clock_config();
 
     infrared_setup();
     buzzer_setup();
